f1c2342e812f1cf5782549a864ce465fbcbac264
musab dogan <musab@musabs-MacBook-Pro.local>
2025-06-15 01:16:42 +0300

    fix: update favicon with official Elasticsearch logo from svgrepo

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" type="image/svg+xml" href="/elasticsearch-icon.svg" />
-        <link rel="icon" type="image/png" href="/elasticsearch-icon.png" />
+        <link rel="icon" type="image/svg+xml" href="/elastic-svgrepo-com.svg" />
         <meta name="description" content="Elasticsearch Query Compare Tool" />
         <meta name="theme-color" content="#1a73e8" />
       </Head>
